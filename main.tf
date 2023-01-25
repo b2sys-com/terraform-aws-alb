ae3d8ce1bf876b79784d23cb7e716caab0230913
diegosorace <dsorace@prismamp.com>
2023-01-25 14:46:55 -0300

    update

--- main.tf
+++ main.tf
@@ -275,8 +275,8 @@ resource "aws_lb_listener_rule" "https_listener_rule" {
     ]
 
     content {
-      type             = action.value["type"]
-      target_group_arn = aws_lb_target_group.main[lookup(action.value, "target_group_index", count.index)].id
+      type             = action_rule.value["type"]
+      target_group_arn = aws_lb_target_group.main[lookup(action_rule.value, "target_group_index", count.index)].id
     }
   }
 
